52140818d56cb5c84c011003ca23278d9667abb8
Hellheym <venganzo@mail.ru>
2019-03-31 00:17:06 +0300

    Another solved problem! Yoohoo!!! +

---
+++ Problems/10001th.py
@@ -0,0 +1,19 @@
+import itertools
+
+def prime(x):
+    if x >= 2:
+        for y in range(2, x):
+            if not (x % y):
+                return False
+    else:
+        return False
+    return True
+
+primes = []
+while len(primes) < 10001:
+    for i in itertools.count():
+        if prime(i):
+            primes.append(i)
+        if len(primes) == 10001:
+            break
+print(primes[-1])
--- Problems/sum_square_difference.py
+++ Problems/sum_square_difference.py
@@ -1,13 +1,13 @@
 def sumofsquares(x):
     a = 0
     for i in range(1, x+1):
-        a = a + i**2
+        a += i**2
     return a
 
 def squareofsum(y):
     b = 0
     for i in range(1, y+1):
-        b = b + i
+        b += i
     return b**2
 
 z = 100
